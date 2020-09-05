f4c40270d53f569b28b20959c0755398c39c54ee
YA!福茂 <yaffmo@gmail.com>
2020-09-05 08:03:44 +0800

    v2

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -11,7 +11,7 @@ export const NavBar = () => {
             alt="logo"
             style={{ marginRight: "10px" }}
           />
-          Reactivity
+          Reactivity NEW
         </Menu.Item>
         <Menu.Item name="messages" />
         <Menu.Item header>
